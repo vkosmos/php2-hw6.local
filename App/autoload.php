176682a8ee2fe536661b85a5514baff6a9fd8eca
vkosmos <korolev.dm.va@gmail.com>
2019-04-03 17:08:24 +0300

    Подключен Twig

--- App/autoload.php
+++ App/autoload.php
@@ -1,12 +1,11 @@
 <?php
 
-function __autoload($class)
-{
+require __DIR__ . '/../vendor/autoload.php';
+
+spl_autoload_register(function ($class){
     $file = str_replace('\\', '/', $class) . '.php';
     $file = __DIR__ . '/../' .$file;
-
     if (file_exists($file)){
         include $file;
     }
-
-}
+} );
